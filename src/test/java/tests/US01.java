6b80a26c2ce649e74d07c98102084866ada56470
GoldenMEmre <goldenpie60@gmail.com>
2026-06-29 21:27:23 +0300

    first commit

---
+++ src/test/java/tests/US01.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.adminPages;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US01 {
+
+    adminPages adminPages=new adminPages();
+
+    @Test
+    public void US01(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        adminPages.signInButton.click();
+
+        Driver.quitDriver();
+
+    }
+
+}
